0e223abd8d976357c64f1344bb97404cb421a71d
Syeda-Nabila-Akter <nabilamoumy@gmail.com>
2023-09-06 20:53:23 +0600

    transformation halfway

---
+++ transformations.py
@@ -0,0 +1,24 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('Resources/Photos/cat.jpg')
+
+cv.imshow('Cat', img)
+
+#transformation
+def translate(img, x, y):
+    transMat = np.float32([[1,0,x],[0,1,y]])
+    dimensions = (img.shape[1], img.shape[0])
+    return cv.warpAffine (img, transMat, dimensions)
+
+#-x --> left
+#-y --> up
+#x --> right
+#y --> down
+
+translated = translate(img, 100, 100)
+cv.imshow('Translated', translated)
+
+
+#rotation
+cv.waitKey(0)
