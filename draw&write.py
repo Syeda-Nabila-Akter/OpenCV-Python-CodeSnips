a2770393d3e479c2b5ca71436c910576f6c61033
Syeda-Nabila-Akter <nabilamoumy@gmail.com>
2023-09-06 12:35:15 +0600

    done with draw and write

--- draw&write.py
+++ draw&write.py
@@ -12,4 +12,21 @@ cv.imshow('Green', blank)
 blank[200:300, 300:400] = 0,0,255
 cv.imshow('Red in Black', blank)
 
+#draw rectangle
+cv.rectangle(blank, (0,0), (250,250), (0,255,0), thickness = 2)
+cv.imshow('Rectangle', blank)
+
+#draw circle
+cv.circle(blank, (255, 255), 40, (0,0,255), thickness=-1)
+cv.imshow('Circle', blank)
+
+#draw line
+cv.line(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2), (0,255,0), thickness=1)
+cv.imshow('Line', blank)
+
+#Write text on image
+cv.putText(blank, 'Hello world', (225,225), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0,255,0), thickness=2)
+cv.imshow('Text', blank)
+
+
 cv.waitKey(0)
--- read.py
+++ read.py
@@ -6,26 +6,21 @@ import cv2 as cv
 
 ####### reading images ########
 #reading large images is a bit challenging (i.e., cat_large.jpg) due to memory limitations but it can be handled in many ways such as by resizing the image
-#img = cv.imread('Resources/Photos/cat.jpg')
-#cv.imshow('Cat', img)
-#cv.waitKey(0)
+img = cv.imread('Resources/Photos/cat.jpg')
+cv.imshow('Cat', img)
+cv.waitKey(0)
 
 ######## reading videos ########
-# capture = cv.VideoCapture('Resources/Videos/dog.mp4')
-# while True:
-#     isTrue, frame = capture.read()
-#     cv.imshow('Video', frame)
+capture = cv.VideoCapture('Resources/Videos/dog.mp4')
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Video', frame)
 
-#     if cv.waitKey(20) & 0xFF==ord('a'):
-#         break
+    if cv.waitKey(20) & 0xFF==ord('a'):
+        break
 
-# capture.release()
-# cv.destroyAllWindows()
+capture.release()
+cv.destroyAllWindows()
 
 
 
-#######################################################
-####### Resizing and Rescaling images & videos ########
-#######################################################
-
-def rescaleFrame(frame, scale=0.75)
