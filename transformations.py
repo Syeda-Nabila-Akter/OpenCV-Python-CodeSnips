d66b43ad3a363b8ad3a84f7ef57ca7ea89652fcc
Syeda-Nabila-Akter <nabilamoumy@gmail.com>
2023-09-06 23:25:32 +0600

    transformations

--- transformations.py
+++ transformations.py
@@ -21,4 +21,28 @@ cv.imshow('Translated', translated)
 
 
 #rotation
+def rotate(img, angle, rotPoint=None):
+
+    (height, width) = img.shape[:2]
+
+    if rotPoint is None:
+        rotPoint = (width//2, height//2)
+        rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
+        dimensions =(width, height)
+
+        return cv.warpAffine(img, rotMat, dimensions)
+rotated = rotate(img, 45)
+cv.imshow ('Rotated', rotated)
+
+#Resizing
+resize = cv.resize(img, (500, 500), interpolation=cv.INTER_CUBIC)
+cv.imshow ('Resized', resize)
+
+#Fliping
+flip = cv.flip(img, 0)
+cv.imshow ('Flipped', flip)
+
+flip = cv.flip(img, 1)
+cv.imshow ('Flipped', flip)
+
 cv.waitKey(0)
